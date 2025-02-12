34e9d9628f6ec4d8b25d73911a99f00a3cc676a9
yoshasudungan <yosua.hasudungan@gmail.com>
2025-02-12 12:39:19 +0700

    add new sleep_record_controller and finishing simple index and create

---
+++ app/controllers/sleep_records_controller.rb
@@ -0,0 +1,40 @@
+class SleepRecordsController < ApplicationController
+  def index
+    if params["user_id"].present?
+      @sleep_records = SleepRecord.where(user_id: params[:user_id])
+    else  
+      @sleep_records = SleepRecord.all
+    end
+    render json: @sleep_records
+  end
+
+  def create
+    binding.pry
+    @sleep_record = SleepRecord.new(sleep_record_params)
+    if @sleep_record.save
+      render json: @sleep_record, status: :created
+    else
+      render json: @sleep_record.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @sleep_record = SleepRecord.find(params[:id])
+    if @sleep_record.update(sleep_record_params)
+      render json: @sleep_record
+    else
+      render json: @sleep_record.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @sleep_record = SleepRecord.find(params[:id])
+    render json: @sleep_record
+  end
+
+  private
+
+  def sleep_record_params
+    params.require(:sleep_record).permit(:clock_in, :clock_out, :user_id)
+  end
+end
