32a4ee016fb550f6bbe2338cedc5220391f1ff0e
yoshasudungan <yosua.hasudungan@gmail.com>
2025-02-13 10:39:06 +0700

    update invalidate cacheif there are any changes on the follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,8 +3,12 @@ class Follow < ApplicationRecord
   belongs_to :followed, class_name: "User"
 
   validates :follower, presence: true
-  validate :follower_and_followed_cannot_be_the_same
+  validates :followed, presence: true
+  validate  :follower_and_followed_cannot_be_the_same
   validates :follower_id, uniqueness: { scope: :followed_id, message: "the same followed_id and follower_id already been taken" }
+
+  # Invalidate cache after a follow is created or destroyed
+  after_commit :invalidate_cache_for_follower, on: [:create, :destroy]
 
   private
 
@@ -13,5 +17,9 @@ class Follow < ApplicationRecord
       errors.add(:follower_id, "can't be the same as followed id")
     end
   end
-  validates :followed, presence: true
+
+  def invalidate_cache_for_follower
+    cache_key = "sleep_records_follower_#{follower_id}_last_week"
+    Rails.cache.delete(cache_key)
+  end
 end
